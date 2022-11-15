5da3a3b065318e05ee3b7b55d343799106b16c15
rusin <rusin095@yahoo.com>
2022-11-15 17:07:12 +0500

    patch2

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import time
 
 
+def print_wait(output_string, letter):
+    print(output_string + letter)
+    time.sleep(0.05)
+
+
 input_string = 'Hello World!'
-letters = 'abcdefghijklmnopqrstuvwxyz'
+letters = string.ascii_lowercase
 output_string = ''
 
 for char in input_string:
     if letters.find(char) != -1:
-        iterator = iter(letters[:letters.find(char)])
-        output_string = output_string + char
-        for letter in iterator:
-            print(output_string + letter)
-            time.sleep(0.04)
+        iterator = letters[:letters.find(char)+1]
+        list(map(lambda i: print_wait(output_string, i), iterator))
+        output_string += char
     else:
-        output_string = output_string + char
+        output_string += char
         print(output_string)
