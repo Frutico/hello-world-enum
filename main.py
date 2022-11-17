72626d1aa5a4d4c3a47682fd189ef3c0d8f13afb
Vadim-GitHub <Vadim-GitHub@users.noreply.github.com>
2022-11-17 06:02:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 S55S = ''
 
 
-def SS55(S5S5, S5SS):
+def sS55(S5S5, S5SS):
     global S55S
     print(S55S + S5S5)
     time.sleep(0.05)
@@ -13,4 +13,4 @@ def SS55(S5S5, S5SS):
 
 
 SS5S = 'Hello World!'
-list(map(lambda S5S5: list(map(lambda S55S: SS55(S55S, S5S5), "".join(sorted(set(string.ascii_lowercase + S5S5))).split(S5S5)[0] + S5S5)), SS5S))
+list(map(lambda S5S5: list(map(lambda S55S: sS55(S55S, S5S5), "".join(sorted(set(string.ascii_lowercase + S5S5))).split(S5S5)[0] + S5S5)), SS5S))
